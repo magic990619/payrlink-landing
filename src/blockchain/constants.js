848afaca877fe1aae62746176aa248490afa1ba7
IntelMin <intelmin0915@gmail.com>
2021-06-03 15:38:32 -0400

    update to mainnet

--- src/blockchain/constants.js
+++ src/blockchain/constants.js
@@ -1,17 +1,20 @@
 
-export const chainId = 42;
+export const chainId = 1;
 
-export const rpcUrl = "https://kovan.infura.io/v3/2377373e9cc84228a6cea33645b511ea";
+export const rpcUrl = {
+  42: "https://kovan.infura.io/v3/2377373e9cc84228a6cea33645b511ea",
+  1: "https://mainnet.infura.io/v3/2377373e9cc84228a6cea33645b511ea"
+}
 //export const rpcUrl = "wss://kovan.infura.io/ws/v3/2377373e9cc84228a6cea33645b511ea";
 
 export const addresses = {
   payrtoken: {
     42: '0xA513b43cfc1fDfF5288F85871D129a66Dcfd69FC',
-    1: '0x0',
+    1: '0xC93d74B2CBCCD0995b8214b38E15fCb4bf842220',
   },
   crowdsale: {
     42: '0x09F4eD5c4Ef70A8Ef6CCeB7F662A210Ea2b6246C',
-    1: '0x0',
+    1: '0x572076d3e7E57562a5318E44Fd8E0Acb62DEe106',
   },
   weth: {
     42: '0xa050886815cfc52a24b9c4ad044ca199990b6690',
